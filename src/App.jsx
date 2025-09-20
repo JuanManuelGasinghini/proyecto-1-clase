1aa444cf088857bd33a1c7e9ffa110344e155413
Juan Manuel Gasinghini <jmgasinghini@gmail.com>
2025-09-20 10:47:33 -0300

    App para publicar

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+  const Buttom = ({label, onClick})=>{
+    return <button onClick={onClick}>{label}</button>
+  }
+
+  const label = 'dale, dale con el look'
+  const onClick = () => alert('en un avión!')
+
+  
+
+  return (
+    <div>
+      <h2>Hola Mundo!</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique voluptatum facere corrupti officiis dicta alias suscipit reprehenderit expedita eligendi et, modi dolorum, nihil molestiae consequuntur, praesentium inventore odio temporibus commodi!</p>
+    <button onClick={onClick}>{label}</button>
+        </div>
+  )
+}
+
+
+
+export default App
