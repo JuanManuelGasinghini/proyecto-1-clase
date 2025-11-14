96deb936946a616c54c1013c03235e567c871e77
Juan Manuel Gasinghini <jmgasinghini@gmail.com>
2025-11-14 13:32:23 -0300

    forrm agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Cart from "./components/Cart/Cart";
+import CheckOut from "./components/CheckOut/CheckOut";
+
 import './App.css'
 
 
@@ -19,6 +22,7 @@ function App() {
           <Route path="/category/:categoryId" element={<ItemListContainer greeting={ "Bienvenidos a la tienda" } />} />
           <Route path="/detail/:id" element={<ItemDetailContainer />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<CheckOut />} />
           <Route path="*" element={ <div>404 NOT FOUND</div> } />
         </Routes>
         </CartProvider>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import { cartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
+import { BsFillTrash3Fill } from "react-icons/bs";
 import "./Cart.css"
 
 const Cart = () => {
   const { cart, deleteProductById, totalPrice, deleteCart } = useContext(cartContext);
-if (cart.length === 0) {
+
+  if (cart.length === 0) {
   return(
     <div className="CarritoVacio">
       <h2 className="CarritoVacioTitulo">No hay productos en el carrito</h2>
@@ -25,8 +27,10 @@ if (cart.length === 0) {
 <p>{productCart.name}</p>
 <p>precio c/u : {productCart.price} </p>
 <p>cantidad: {productCart.quantity}</p>
-<p>precio parcial: {productCart.price * productCart.quantity}</p>
-          <button onClick={() => deleteProductById(productCart.id)} className="EliminarVsta">Eliminar</button>
+<p>precio parcial: ${productCart.price * productCart.quantity}</p>
+          <button onClick={() => deleteProductById(productCart.id)} className="EliminarVsta">Eliminar
+            <BsFillTrash3Fill />
+          </button>
           </div>
           </div>
 
@@ -34,6 +38,7 @@ if (cart.length === 0) {
       }
       <div className="CierreDelCarrito">
       <h2 className="CPrecioTotal">Precio Total: {totalPrice()}</h2>
+      <Link to="/checkout" className="BotonDeCierre">Finalizar Compra</Link>
       <button onClick={deleteCart} className="BotonDeCierre">Vaciar Carrito</button>
       </div>
     </div>
