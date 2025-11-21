5e11cba8d51427699e2a5e4ecb3f9bd903fd243a
Juan Manuel Gasinghini <jmgasinghini@gmail.com>
2025-11-21 20:38:02 -0300

    accesorios  de seo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Proyecto Final de la Comisión 88020 del curso de React Js de Coderhouse
+E-commerce
 Alumno Gasinghini Juan Manuel
--- src/components/Itemlist/Itemlist.jsx
+++ src/components/Itemlist/Itemlist.jsx
@@ -1,5 +1,5 @@
 import Item from '../Item/Item';
-import './itemlist.css';
+import './Itemlist.css';
 
 const Itemlist = ({ products }) => {
   return (
